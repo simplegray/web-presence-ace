f456ffc1905774a7c9f459ddbd9a795b465f0a38
Hunter <simplegray@gmail.com>
2026-02-02 18:51:29 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/web-presence-ace/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +8,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
